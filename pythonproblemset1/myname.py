2459ca85ef1b8491126d3f454b22cd90c72d8d73
info <info@info03.cshl.edu>
2019-10-16 11:21:05 -0400

    complete myname python script

--- pythonproblemset1/myname.py
+++ pythonproblemset1/myname.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
-print('My name : Becky')
+import sys  #need to import sys to use input from command line
+
+name = sys.argv[1] #assigning name variable to the 1st input
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name :', name+  '\n' # '\n' will add a line break, the + doesn't leave a space, a comma between print arguments will add a space
+'My favorite color:', color+  '\n'
+'My favorite activity:', activity+ '\n'
+'My favorite animal:', animal)
+
 
